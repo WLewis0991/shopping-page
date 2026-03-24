30922b174956ee88fa75cf7959fd92d67e636cf0
wlewis0991 <wlewis1990@gmail.com>
2026-03-24 09:25:32 -0700

    Homepage layout added with button to link products page. API to get products added to app page  and currently fetching all available items and displayed in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 
+fetch('https://fakestoreapi.com/products/')
+            .then(res=>res.json())            
+            .then(json=>console.log(json))
+
 function App() {
   return (
     <BrowserRouter>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
+import { Link } from "react-router-dom"
+
 export default function Homepage() {
 
     return(<>
-    <p>HomePage</p>
+    <div className="homepage-body">
+        <div className="homepage-container">
+            <div className="homepage-info">
+            <Link to="products"> <button>Shop Now</button></Link>
+            </div>
+            <div className="homepage-image">
+
+            </div>
+        </div>
+    </div>
     </>)
 }
