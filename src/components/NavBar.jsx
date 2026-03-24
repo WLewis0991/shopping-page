fbc52cfcefa4e335ce99233e06b8e39c575615c7
wlewis0991 <wlewis1990@gmail.com>
2026-03-24 08:02:31 -0700

    Nav bar cleaned up and not found page now includes button to return home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,11 +3,12 @@
  export default function NavBar(){
 
     return(<>
-    <p>NavBar</p>
-      <div>
-      <Link to="/">Home</Link>
-      <Link to="/products">Products</Link>
-      <Link to="/cart">Cart</Link>
+      <div className="navbar-div">
+        <div className="nav-links">
+          <Link to="/" className="nav-button">Home</Link>
+          <Link to="/products" className="nav-button">Products</Link>
+          <Link to="/cart" className="nav-button">Cart</Link>
+        </div>
       </div>
     </>)
  }
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router-dom"
 export default function NotFound(){
 
     return(<>
-    <div>404 page not found</div>
+    <div className="not-found-page">
+        <p>404 page not found </p>
+        <Link to="/">
+        <button>Return Home</button>
+        </Link>
+    </div>
     </>)
 }
