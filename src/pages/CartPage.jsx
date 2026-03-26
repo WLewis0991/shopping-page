bf28dd1d047878e10187c8221bc6b3c42aa61989
wlewis0991 <wlewis1990@gmail.com>
2026-03-26 14:18:52 -0700

    Cart page now shows cart total, shipping, tax based off cart and a grand total

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,13 +5,22 @@ export default function CartPage(props){
 
   const { cart , setCart} = props
 
-function findTotal() {
+function findCartTotal() {
   let total = 0;
   cart.forEach(i => {
     total += i.quantity * i.price;
   });
   return total;
 }
+
+function findTax(){
+  return (findCartTotal() * 0.0753).toFixed(2)
+}
+
+function findTotal() {
+  return Number(findTax()) + 10 + Number(findCartTotal().toFixed(2));
+}
+
 
   if (cart.length === 0) {
   return (
@@ -32,7 +41,12 @@ function findTotal() {
               <CartCard key={item.id} item={item} cart={cart} setCart={setCart}/> 
             ))}
             </ul>
-            <div className="cart-total-container">Total is {findTotal().toFixed(2)}</div>
+            <div className="cart-total-container">
+              <p>Cart total : {findCartTotal().toFixed(2)}</p>
+              <p>Shipping : $10.00 </p>
+              <p>Tax (7.53%) : {findTax()} </p>
+              <h3>Total: {findTotal().toFixed(2)}</h3>
+            </div>
           </div>
     </div>
     </>)
